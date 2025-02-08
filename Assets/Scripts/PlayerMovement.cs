8feadce8543025f875a6ed8fe81c08d740c162b0
Alexey Dmitriev <alex717nd@gmail.com>
2025-02-08 12:31:46 -0800

    Horizontal Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,22 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Values")]
+    [SerializeField] private float speed;
+
+    [Header("References")]
     [SerializeField] private InputActionAsset inputSystem;
     private InputAction horizontalMovement;
     private float horizontalMovementValue;
 
     private void OnEnable()
     {
-        
+        // Find input actions
+        var playerControlls = inputSystem.FindActionMap("Player");
+        horizontalMovement = playerControlls.FindAction("HorizontalMovement");
 
         horizontalMovement.Enable();
     }
@@ -19,18 +26,17 @@ public class PlayerMovement : MonoBehaviour
         horizontalMovement.Disable();
     }
 
-    private void Start()
-    {
-        // Find input actions
-        var playerControlls = inputSystem.FindActionMap("Player");
-        horizontalMovement = playerControlls.FindAction("HorizontalMovement");
-    }
-
     private void Update()
     {
         // Get input value
         horizontalMovementValue = horizontalMovement.ReadValue<float>();
 
-        Debug.Log(horizontalMovementValue);
+        ApplyHorizontalMovement();
+    }
+
+    private void ApplyHorizontalMovement()
+    {
+        float moveValue = horizontalMovementValue * speed * Time.deltaTime;
+        transform.position += new Vector3(moveValue, 0, 0);
     }
 }
