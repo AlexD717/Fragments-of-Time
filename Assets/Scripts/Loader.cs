44057bf75ebd796f89cdc0d9706b889a83516a30
Alexey Dmitriev <alex717nd@gmail.com>
2025-03-03 10:11:03 -0800

    In Between Loading Screen

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -1,10 +1,26 @@
-using UnityEngine;
+using System;
 using UnityEngine.SceneManagement;
 
 public static class Loader
 {
+    private static Action onLoaderCallback;
+
    public static void Load(string scene)
     {
-        SceneManager.LoadScene(scene);
+        SceneManager.LoadScene("LoadingScreen");
+
+        onLoaderCallback = () =>
+        {
+            SceneManager.LoadScene(scene);
+        };
+    }
+
+    public static void LoaderCallback()
+    {
+        if (onLoaderCallback != null)
+        {
+            onLoaderCallback();
+            onLoaderCallback = null;
+        }
     }
 }
