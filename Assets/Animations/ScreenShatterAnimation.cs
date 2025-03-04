328147f9025e207a8717e64ce85f41bc2028127a
Alexey Dmitriev <alex717nd@gmail.com>
2025-03-03 17:50:29 -0800

    Screen Shatter Animation

---
+++ Assets/Animations/ScreenShatterAnimation.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class ScreenShatterAnimation : MonoBehaviour
+{
+    [SerializeField] private Vector2 screenShatterWidth;
+    [SerializeField] private int numTotalShatters;
+    [SerializeField] private GameObject screenShatterPrefab;
+    private bool screenShatterAnimStarted = false;
+    private int numShatters = 0;
+    [SerializeField] private float startingTime;
+    [SerializeField] private float r;
+    private float nextShatterTime = 0f;
+
+    private string nextLevelToLoad;
+
+    [SerializeField] private RectTransform canvas;
+
+    private void Update()
+    {
+        if (!screenShatterAnimStarted) return;
+
+        if (Time.time >= nextShatterTime)
+        {
+            numShatters++;
+            SpawnScreenShatter();
+            if (numShatters >= numTotalShatters)
+            {
+                Loader.Load(nextLevelToLoad);
+            }
+            nextShatterTime = Time.time + Mathf.Pow(startingTime*(1-r), numShatters);
+        }
+    }
+
+    public void ShatterScreen(string level)
+    {
+        screenShatterAnimStarted = true;
+        nextLevelToLoad = level;
+    }
+
+    private void SpawnScreenShatter()
+    {
+
+        // Gets a random point on the canvas 
+        Vector3 screenPosition = new Vector3(Random.Range(0, canvas.rect.width/2), Random.Range(0, canvas.rect.height/2), 0);
+
+        GameObject screenShatter = Instantiate(screenShatterPrefab, screenPosition, Quaternion.Euler(0, 0, Random.Range(0, 360)));
+        screenShatter.transform.SetParent(canvas, true);
+        RectTransform rectTransform = screenShatter.GetComponent<RectTransform>();
+        rectTransform.sizeDelta = new Vector2(rectTransform.rect.width, Random.Range(screenShatterWidth.x, screenShatterWidth.y));
+    }
+}
--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -1,9 +1,23 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class MainMenuUI : MonoBehaviour
 {
     public void LoadLevel(string levelNum)
     {
-        Loader.Load("Level"+levelNum);
+        Loader.Load("Level" + levelNum);
+    }
+
+    public void LoadScreenShatterLevel(string levelNum)
+    {
+        ScreenShatterAnimation screenShatterAnimation = FindFirstObjectByType<ScreenShatterAnimation>();
+        if (screenShatterAnimation != null)
+        {
+            screenShatterAnimation.ShatterScreen("Level" + levelNum);
+        }
+        else
+        {
+            Loader.Load("Level" + levelNum);
+        }
     }
 }
