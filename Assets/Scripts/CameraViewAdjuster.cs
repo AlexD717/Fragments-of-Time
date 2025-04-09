d01823457cfe5b4cbdcab57d3f7832afd5d15b00
Alexey Dmitriev <alex717nd@gmail.com>
2025-04-09 10:19:16 -0700

    Camera Centers

--- Assets/Scripts/CameraViewAdjuster.cs
+++ Assets/Scripts/CameraViewAdjuster.cs
@@ -1,5 +1,6 @@
 using Unity.Cinemachine;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class CameraViewAdjuster : MonoBehaviour
 {
@@ -7,32 +8,51 @@ public class CameraViewAdjuster : MonoBehaviour
     [SerializeField] private float baseZoom;
     [SerializeField] private Vector2 zoomOffset;
     [SerializeField] private float zoomSpeed;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float cameraZPos;
 
+    private List<GameObject> targets;
     private GameObject playerTimeTraveled;
     private CinemachineCamera vcam;
+
+    private Vector3 velocity;
 
     private void Start()
     {
         vcam = GetComponent<CinemachineCamera>();
+
+        targets = new List<GameObject>();
+
+        playerTimeTraveled = GameObject.FindGameObjectWithTag("PlayerTimeTraveled");
+        if (playerTimeTraveled != null)
+        {
+            targets.Add(playerTimeTraveled);
+        }
+        targets.Add(GameObject.FindGameObjectWithTag("Player"));
     }
 
-    private void Update()
+    private void LateUpdate()
     {
-        if (playerTimeTraveled == null)
-        {
-            playerTimeTraveled = GameObject.FindGameObjectWithTag("PlayerTimeTraveled");
-        }
+        targets.RemoveAll(item => item == null);
+        if (targets.Count <= 0) { return; }
+
+        AdjustCameraPosition();
         AdjustCameraView();
+    }
+
+    private void AdjustCameraPosition()
+    {
+        Vector3 centerPoint = GetCenterPoint();
+        Vector3 newPosition = new Vector3(centerPoint.x, centerPoint.y, cameraZPos);
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, moveSpeed);
     }
 
     private void AdjustCameraView()
     {
+        Vector2 boundsSize = GetGreatestDistance();
+        float sizeX = boundsSize.x / zoomOffset.x;
+        float sizeY = boundsSize.y / zoomOffset.y;
         Vector2 distance = Vector2.zero;
-        if (playerTimeTraveled != null)
-            distance = new Vector2(Mathf.Abs(transform.position.x - playerTimeTraveled.transform.position.x), Mathf.Abs(transform.position.y - playerTimeTraveled.transform.position.y));
-        
-        float sizeX = distance.x / zoomOffset.x;
-        float sizeY = distance.y / zoomOffset.y;
 
         if (sizeX > sizeY)
         {
@@ -58,5 +78,32 @@ public class CameraViewAdjuster : MonoBehaviour
             }
             vcam.Lens.OrthographicSize = Mathf.Lerp(vcam.Lens.OrthographicSize, sizeY, Time.deltaTime * zoomSpeed);
         }
+    }
+
+    private Vector3 GetCenterPoint()
+    {
+        if (targets.Count == 0)
+        {
+            return targets[0].transform.position;
+        }
+
+        var bounds = new Bounds(targets[0].transform.position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].transform.position);
+        }
+
+        return bounds.center;
+    }
+
+    private Vector2 GetGreatestDistance()
+    {
+        var bounds = new Bounds(targets[0].transform.position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].transform.position);
+        }
+
+        return new Vector2(bounds.size.x, bounds.size.y);
     }
 }
